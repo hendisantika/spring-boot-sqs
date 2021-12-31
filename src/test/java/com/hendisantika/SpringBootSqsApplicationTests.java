41daf3023f5d812b658311fb8b96c1b2a9071174
Hendi Santika <hendisantika@gmail.com>
2022-01-01 05:44:30 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/SpringBootSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
