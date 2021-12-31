e4aa12843d0828206c61ec8ca90d4ff56bab61e3
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:03:35 +0700

    #15 - Add StatusCode Enum

---
+++ src/main/java/com/hendisantika/model/StatusCode.java
@@ -0,0 +1,17 @@
+package com.hendisantika.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sqs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/01/22
+ * Time: 06.03
+ */
+public enum StatusCode {
+    SUCCESS,
+    ERROR,
+    PENDING,
+    WARNING
+}
