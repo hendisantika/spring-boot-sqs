b09baa40f5a1c2235bcd07676090a8055fadc6e6
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:16:42 +0700

    #35 - Add jmsListenerContainerFactory in JmsConfig

--- src/main/java/com/hendisantika/sqs/JmsConfig.java
+++ src/main/java/com/hendisantika/sqs/JmsConfig.java
@@ -3,10 +3,14 @@ package com.hendisantika.sqs;
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
 
 import javax.annotation.PostConstruct;
+import javax.jms.Session;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +34,15 @@ public class JmsConfig {
         ProviderConfiguration providerConfiguration = new ProviderConfiguration();
         connectionFactory =
                 new SQSConnectionFactory(providerConfiguration, amazonSQSClient.getClient());
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(this.connectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("3-10");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        return factory;
     }
 }
