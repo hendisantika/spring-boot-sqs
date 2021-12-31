8c1ac01961f7c89063227f5f77f60ab1ce1cd871
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:13:53 +0700

    #31 - Add AmazonSqsClient

--- src/main/java/com/hendisantika/sqs/AmazonSqsClient.java
+++ src/main/java/com/hendisantika/sqs/AmazonSqsClient.java
@@ -1,9 +1,14 @@
 package com.hendisantika.sqs;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -22,6 +27,12 @@ public class AmazonSqsClient {
 
     private AmazonSQS client;
 
+    @Value("${aws.access.key}")
+    private String ACCESS_KEY;
+
+    @Value("${aws.secret.key}")
+    private String SECRET_KEY;
+
     @PostConstruct
     private void initializeAmazonSqsClient() {
         this.client =
@@ -31,4 +42,12 @@ public class AmazonSqsClient {
                         .build();
     }
 
+    private AWSCredentialsProvider getAwsCredentialProvider() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
+
+    public AmazonSQS getClient() {
+        return client;
+    }
 }
