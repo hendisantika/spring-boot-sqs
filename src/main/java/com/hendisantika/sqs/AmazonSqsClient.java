1792179882e4c11be1574b7d46d6e21614f1429e
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:09:50 +0700

    #30 - Add initializeAmazonSqsClient in AmazonSqsClient

--- src/main/java/com/hendisantika/sqs/AmazonSqsClient.java
+++ src/main/java/com/hendisantika/sqs/AmazonSqsClient.java
@@ -1,7 +1,12 @@
 package com.hendisantika.sqs;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +21,14 @@ import org.springframework.stereotype.Service;
 public class AmazonSqsClient {
 
     private AmazonSQS client;
+
+    @PostConstruct
+    private void initializeAmazonSqsClient() {
+        this.client =
+                AmazonSQSClientBuilder.standard()
+                        .withCredentials(getAwsCredentialProvider())
+                        .withRegion(Region.getRegion(Regions.AP_SOUTHEAST_1).getName())
+                        .build();
+    }
+
 }
