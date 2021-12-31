4dd8526cbe48933491241576df59fbf794c7dd6b
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:07:19 +0700

    #23 - Add Util

---
+++ src/main/java/com/hendisantika/util/Util.java
@@ -0,0 +1,17 @@
+package com.hendisantika.util;
+
+import org.modelmapper.ModelMapper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sqs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/01/22
+ * Time: 06.07
+ */
+public class Util {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+}
