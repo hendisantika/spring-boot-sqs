57143d3e8c52ee129a132ba843a6190b75b06218
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:05:32 +0700

    #19 - Add requestErrorHandler Utils

---
+++ src/main/java/com/hendisantika/util/ErrorUtil.java
@@ -0,0 +1,41 @@
+package com.hendisantika.util;
+
+import com.hendisantika.model.ErrorModel;
+import com.hendisantika.model.ServiceResponse;
+import com.hendisantika.model.StatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sqs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/01/22
+ * Time: 06.05
+ */
+public class ErrorUtil {
+    public static ServiceResponse requestErrorHandler(BindingResult bindingResult) {
+        ServiceResponse response = new ServiceResponse();
+        List<ErrorModel> errorModelList = new ArrayList();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setField(fieldError.getField());
+            errorModel.setMessage(fieldError.getDefaultMessage());
+            errorModel.setDescription(fieldError.getCode());
+            errorModelList.add(errorModel);
+        }
+
+        response.setErrorList(errorModelList);
+        response.setStatus(HttpStatus.BAD_REQUEST);
+        response.setStatusCode(StatusCode.ERROR);
+        return response;
+    }
+
+}
