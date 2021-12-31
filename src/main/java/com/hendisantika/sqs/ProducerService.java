0f9ba7110fcfebdbed5cab9d029e9abcc7176007
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:18:22 +0700

    #39 - Add sendMultiple in ProducerService

--- src/main/java/com/hendisantika/sqs/ProducerService.java
+++ src/main/java/com/hendisantika/sqs/ProducerService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.sqs;
 
+import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +31,21 @@ public class ProducerService {
     public void send(String queueName, String requestBody) {
         LOG.info("Send to {} message --> {}", queueName, requestBody);
         defaultJmsTemplate.convertAndSend(queueName, requestBody);
+    }
+
+    public void sendMultiple() {
+        SendMessageBatchRequestEntry[] dataArray = new SendMessageBatchRequestEntry[10];
+        for (int i = 0; i < 10; i++) {
+            dataArray[i] =
+                    new SendMessageBatchRequestEntry(UUID.randomUUID().toString(), "Hello from message " + i);
+        }
+
+        LOG.info("Sending batch message to order queue");
+
+        SendMessageBatchRequest sendBatchRequest =
+                new SendMessageBatchRequest()
+                        .withQueueUrl("queue-url")
+                        .withEntries(dataArray);
+        amazonSQSClient.getClient().sendMessageBatch(sendBatchRequest);
     }
 }
