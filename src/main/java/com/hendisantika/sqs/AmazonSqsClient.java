497364a6c5475efd2ebaf4c16da4a19242704b43
Hendi Santika <hendisantika@gmail.com>
2022-01-01 06:09:31 +0700

    #29 - Add AmazonSqsClient

---
+++ src/main/java/com/hendisantika/sqs/AmazonSqsClient.java
@@ -0,0 +1,19 @@
+package com.hendisantika.sqs;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sqs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/01/22
+ * Time: 06.09
+ */
+@Service
+public class AmazonSqsClient {
+
+    private AmazonSQS client;
+}
